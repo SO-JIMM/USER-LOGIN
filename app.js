1ca1c75556c65898bd1b6b0fd915a7ea561b89a2
SO-JIMM <jamesdgrace@gmail.com>
2019-12-06 20:51:29 +1100

    user model schema

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const exphbs  = require('express-handlebars')
 const path = require('path')
 
 const app = express()
-
+require('./mongoose/mongoose')
 const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, './public')
@@ -15,6 +15,14 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => {
   res.render('home', {title: 'Home'})
+})
+
+app.get('/create-user', (req, res) => {
+  res.render('create-user', {title: 'Create User'})
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', {title: 'Login'})
 })
 
 app.listen(port, () => {
